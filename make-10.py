3f7bb7ce4f1e17d4f1696ddb560da2bcb999c7d6
ishopeatsea <sophie4210@gmail.com>
2024-06-21 17:26:23 +1000

    I did it !

--- make-10.py
+++ make-10.py
@@ -1,6 +1,10 @@
-from enum import Enum
-from itertools import permutations #pairwise, starmap
+from enum import StrEnum
+from itertools import permutations, product #pairwise, starmap
 # import numpy as np
+
+def printlist(list):
+	for x in list:
+		print(x)
 
 def add(x,y):
 	return x+y
@@ -11,6 +15,14 @@ def multiply(x,y):
 def divide (x,y):
 	return x/y
 
+def w(f): # ω is the symbol used in the definition of "operation" - https://en.m.wikipedia.org/wiki/Operation_(mathematics)#Definition 
+	return {
+	'add': '+',
+	'subtract': '-',
+	'multiply': '×',
+	'divide': '÷',
+	}[f.__name__]
+
 def split_digits(input):
 	return [int(c) for c in str(input)]
 	
@@ -20,23 +32,19 @@ funcs = [
 	multiply,
 	divide
 ]
-abcd = split_digits(1234)
-tree = {}
-for a, b in permutations(abcd, 2):
-	print(a,b)
-	tree[a,b] = {}
-	tree[a,b]['slag'] = [x for x in abcd if x not in [a,b]]
-	tree[a,b]['funcs'] = {}
-	for f in funcs:
-		fnym = f.__name__
-		tree[a,b]['funcs'][fnym] = {}
-		tree[a,b]['funcs'][fnym]['result'] = f(a,b)
-	for c in tree[a,b]['slag']:
-		for f, branch in tree[a,b]['funcs'].items():
-			tree[a,b]['funcs'][f]['next'] = {}
-			for g in funcs:
-				gnym = g.__name__
-# fuck this
+digits = split_digits(1234)
+#results = {}
+tens = []
+for a, b, c, d in permutations(digits, 4):
+	#results[a,b,c,d] = {}
+	for f, g, h in product(funcs, repeat=3):
+		print(a, w(f), b, w(g), c, w(h), d, "=", h(g(f(a,b),c),d))
+		#results[a,b,c,d][f,g,h] = h(g(f(a,b),c),d)
+		if h(g(f(a,b),c),d) == 10:
+			#tens.append((a,w(f),b,w(g),c,w(h),d))
+			tens.append(((a,b,c,d),(w(f),w(g),w(h))))
+printlist(tens)
+	
 
 # for x in permutations(digits, 2):
 # 	for f in funcs: 
