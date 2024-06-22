1001812f889a252464487109f580263208b2549d
ishopeatsea <sophie4210@gmail.com>
2024-06-22 11:33:54 +1000

    wrapped it in a function

--- make-10.py
+++ make-10.py
@@ -32,19 +32,29 @@ def w(f): # ω is the symbol used in the definition of "operation" - https://en.
 		'divide': '÷',
 	}[f.__name__]
 
+def get_number():
+	return input("Carriage number: ")
 def split_digits(input):
 	return [int(c) for c in str(input)]
 	
-funcs = funclist()
-number = input("Carriage number: ")
-digits = split_digits(number)
-tens = []
-for a, b, c, d in permutations(digits, 4):
-	for f, g, h in product(funcs, repeat=3):
-		try:
-			if h(g(f(a,b),c),d) == 10:
-				tens.append(((a,b,c,d),(f,g,h)))
-		except ZeroDivisionError:
-			#print("skipped - divide by zero")
-			pass
-printsoln(tens)
+def find_solutions(number, funcs, target):
+	#funcs = funclist()
+	#number = input("Carriage number: ")
+	digits = split_digits(number)
+	solns = []
+	for a, b, c, d in permutations(digits, 4):
+		for f, g, h in product(funcs, repeat=3):
+			try:
+				if h(g(f(a,b),c),d) == target:
+					solns.append(((a,b,c,d),(f,g,h)))
+			except ZeroDivisionError:
+				#print("skipped - divide by zero")
+				pass
+	#printsoln(tens)
+	return solns
+	
+if __name__ == "__main__":
+	number = get_number()
+	while number != "exit":
+		printsoln(find_solutions(number, funclist(), 10))
+		number = get_number()
