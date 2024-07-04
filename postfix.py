a98902df99244ec68b87af7da2c2ae173124a9f2
ishopeatsea <sophie4210@gmail.com>
2024-07-05 08:57:06 +1000

    exploring this grammar ive made

--- postfix.py
+++ postfix.py
@@ -1,70 +1,35 @@
-from nltk.parse.generate import generate#, demo_grammar
+from nltk.parse.generate import generate
 from nltk import CFG
 
-#grammar = CFG.fromstring(demo_grammar)
-#print(demo_grammar)
-#print(grammar)
 postfix_grammar = """
 S -> E E W
 E -> E E W | N
-W -> '+' | '-' | '×' | '÷'
-N -> '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9'
+W -> 'ω'
+N -> '#'
 """
-#print(postfix_grammar)
 grammar = CFG.fromstring(postfix_grammar)
 print(grammar)
 
-sentences = list(generate(grammar, depth=4))
-print([s for s in sentences])
+sentences = list(generate(grammar, depth=8))
+#count = 0
+max = 0
+for s in sentences:
+	print(s)
+	if len(s) > max:
+		max = len(s)
+print(f"longest sentence: {max}")
+#	if sum(1 for t in s if t == '#') == 2:
+#		print(s)
+#		count += 1
+#print(f"total: {count}")
 
+# depth 8 for 6 digits (max len 63 (!))
+# depth 7 for 5 digits (max len 31)
+# depth 6 for 4 digits (max len 15)
+# depth 5 for 3 digits (max len 7)
+# depth 4 for 2 digits (max len 3)
+# depth 3 for none (max len 0)
 
-
-
-#from collections import deque
-#from random import random
-
-#def generate_wordspace(n): # n = number of digitsl
-#	if n < 2:
-#		raise Exception("n must be >= 2")
-#	
-#	wcount = 1
-#	#q = deque("EEW")
-#	q = deque(['E', 'E', 'W'])
-#	while wcount < n-1:
-#		t = q.popleft()
-
-#	ncount = 0
-#	wcount = 1
-#	result = []
-#	q = deque(['E', 'E', 'W'])
-#	while ncount < 4:
-#		#print(string.popleft())
-#		t = q.popleft()
-#		if t == 'N' or t == 'W':
-#			result.append(t)
-#		elif wcount == n-1:
-#			q.appendleft('N')
-#		else:
-#			pass
-#		ncount += 1
-#	
-#generate_postfix(1)
-#	
-#	def parse(t): # token
-#		if token == 'S':
-#			return 'EEW'
-#		elif token == 'E':
-#			if random() < 0.5:
-#				return 'EEW'
-#			else:
-#				return 'N'
-#				
-#	
-#def parse_grammar_token(token):
-#	if token == 'S':
-#		return 'EEW'
-#	else:
-#		return 'N'
 
 """
 S -> E E W
