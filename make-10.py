3966568f36f22f2f08ffa27ffedfa7cbce2c7884
S Ryan <sophie.ryan@uts.edu.au>
2024-07-05 16:39:17 +1000

    making my postfix dreams come true

--- make-10.py
+++ make-10.py
@@ -1,13 +1,23 @@
-from enum import StrEnum
+# from enum import StrEnum
 from itertools import permutations, product #pairwise, starmap
 # import numpy as np
+from nltk.parse.generate import generate
+from nltk import CFG
+# from collections import deque
 
 def printlist(list):
 	for x in list:
 		print(x)
-def printsoln(list):
-	for (a,b,c,d), (f,g,h) in list:
-		print(a,w(f),b,w(g),c,w(h),d,"= 10")
+# def printsoln(list):
+# 	for (a,b,c,d), (f,g,h) in list:
+# 		print(a,w(f),b,w(g),c,w(h),d,"= 10")
+
+def count(thing, list):
+	count = 0
+	for x in list:
+		if x == thing:
+			count += 1
+	return count
 
 def add(x,y):
 	return x+y
@@ -32,29 +42,72 @@ def w(f): # ω is the symbol used in the definition of "operation" - https://en.
 		'divide': '÷',
 	}[f.__name__]
 
+def postfix_grammar():
+	postfix_grammar = """
+	S -> E E W
+	E -> E E W | N
+	W -> 'ω'
+	N -> '#'
+	"""
+	return CFG.fromstring(postfix_grammar)
+
+# https://stackoverflow.com/a/30067622
+def eval_postfix(string):
+	s = [] # stack
+	for t in string: # char
+		if c in "0123456789":
+			s.append(int(c))
+		expr = None
+		if not s.is_empty():
+
+
+
 def get_number():
 	return input("Carriage number: ")
-def split_digits(input):
-	return [int(c) for c in str(input)]
+# def split_digits(input):
+# 	return [int(c) for c in str(input)]
 	
-def find_solutions(number, funcs, target):
+def find_solutions(number, functions, target):
+
+	grammar = postfix_grammar()
+	sentences = [x for x in list(generate(grammar, depth=6)) if sum(1 for t in x if t == "#") == len(number)]
+	numorders = permutations(number, 4)
+	# funcorders = product(functions, repeat=3)
+	# funcorders = product([w(f) for f in functions], repeat=3)
+
+	solns = []
 	#funcs = funclist()
 	#number = input("Carriage number: ")
-	digits = split_digits(number)
-	solns = []
-	for a, b, c, d in permutations(digits, 4):
-		for f, g, h in product(funcs, repeat=3):
-			try:
-				if h(g(f(a,b),c),d) == target:
-					solns.append(((a,b,c,d),(f,g,h)))
-			except ZeroDivisionError:
-				#print("skipped - divide by zero")
-				pass
+	# digits = split_digits(number)
+	# for word in words:
+	# 	print(''.join(word))
+	# for perm in permutations(number, 4):
+		# print(f"{''.join(perm)}")
+	
+	# for sentence,digits,funcs in list(product(sentences, numorders, funcorders)):
+	for sentence,digits in list(product(sentences, numorders)):
+		
+		s = ''.join(sentence)
+		s = s.replace('#',"{}").format(*digits)
+		s = s.replace('ω',"{}").format(*product(functions, repeat=count('ω', s)))
+
+		eval_postfix(s)
+
+		solns.append(s)
+
+		# for f, g, h in product(funcs, repeat=3):
+		# 	try:
+		# 		if h(g(f(a,b),c),d) == target:
+		# 			solns.append(((a,b,c,d),(f,g,h)))
+		# 	except ZeroDivisionError:
+		# 		#print("skipped - divide by zero")
+		# 		pass
 	#printsoln(tens)
 	return solns
 	
 if __name__ == "__main__":
-	number = get_number()
-	while number != "exit":
-		printsoln(find_solutions(number, funclist(), 10))
-		number = get_number()
+	printlist(find_solutions(get_number(), funclist(), 10))
+	# number = get_number()
+	# while number != "exit":
+	# 	printsoln(find_solutions(number, funclist(), 10))
+	# 	number = get_number()
--- postfix.py
+++ postfix.py
@@ -1,34 +1,59 @@
 from nltk.parse.generate import generate
 from nltk import CFG
+import time
 
-postfix_grammar = """
-S -> E E W
-E -> E E W | N
-W -> 'ω'
-N -> '#'
-"""
-grammar = CFG.fromstring(postfix_grammar)
-print(grammar)
+def postfix_grammar():#digits):
+	grammar = [
+	"S -> E E W",
+	"E -> E E W | N",
+	"W -> 'ω'",
+	"N -> '#'"
+	]
+	# for d in digits:
+	# 	grammar.append(f"N -> '{d}'")
+	return CFG.fromstring(grammar)
 
-sentences = list(generate(grammar, depth=8))
-#count = 0
-max = 0
-for s in sentences:
-	print(s)
-	if len(s) > max:
-		max = len(s)
-print(f"longest sentence: {max}")
-#	if sum(1 for t in s if t == '#') == 2:
-#		print(s)
-#		count += 1
-#print(f"total: {count}")
+number = "1234"
 
-# depth 8 for 6 digits (max len 63 (!))
-# depth 7 for 5 digits (max len 31)
-# depth 6 for 4 digits (max len 15)
-# depth 5 for 3 digits (max len 7)
-# depth 4 for 2 digits (max len 3)
-# depth 3 for none (max len 0)
+grammar = postfix_grammar()
+print("\n", grammar, "\n")
+
+words = list(generate(grammar, depth=6))
+for word in words:
+	if sum(1 for t in word if t == "#") == len(number):
+		print(''.join(word))
+
+# for d in range(0,10):
+
+# 	start = time.time()
+
+# 	sentences = list(generate(grammar, depth=d))
+# 	#count = 0
+# 	max_len = 0
+# 	max_w = 0
+# 	for s in sentences:
+# 		# print(s)
+# 		if len(s) > max_len:
+# 			max_len = len(s)
+# 		if sum(1 for t in s if t == 'ω') > max_w:
+# 			max_w = sum(1 for t in s if t == 'ω')
+# 	print(f"d = {d}")
+# 	print(f"longest sentence: {max_len}")
+# 	print(f"most operators: {max_w}")
+# 	#	if sum(1 for t in s if t == '#') == 2:
+# 	#		print(s)
+# 	#		count += 1
+# 	#print(f"total: {count}")
+
+# 	print(f"Time taken: {time.time()-start}''")
+
+# depth 9 ((est.) 63 operators, 2.5 hours to run)
+# depth 8 for 6 digits (max len 63 (!), 31 operators, 12 seconds to run)
+# depth 7 for 5 digits (max len 31, 15 operators, 0.017 seconds to run)
+# depth 6 for 4 digits (max len 15, 7 operators, <0.001 seconds)
+# depth 5 for 3 digits (max len 7, 3 operators, <0.001 seconds)
+# depth 4 for 2 digits (max len 3, 1 operator, <e^-5 seconds)
+# depth <4 for none (max len 0)
 
 
 """
